4b554684baeff90a8a81fc9f91d489b750be6c97
Scott Barnes <scottreidbarnes@gmail.com>
2024-09-19 21:11:54 -0700

    Fix: include `daisy/daisy_files` in `setuptools`

--- setup.py
+++ setup.py
@@ -41,4 +41,4 @@ setup(name='archive-hocr-tools',
           'pdf': ['PyMuPDF==1.22.5', 'numpy==1.21.3'],
           'pagenumber': ['viterbi-trellis==0.0.3', 'roman>=3.3', 'numpy>=1.21.3', 'scikit-learn>=1.2.2'],
       },
-      package_data={'hocr': ['data/*', 'daisy/*']})
+      package_data={'hocr': ['data/*', 'daisy/*', 'daisy/daisy_files/*']})
